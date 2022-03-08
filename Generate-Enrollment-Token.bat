07b928ec51a364f3732c8c1b632c77f5c21eef7b
unni-cs <cs.unnikrishnan10@gmail.com>
2022-03-08 11:01:35 +0530

    updated the copy command

--- Generate-Enrollment-Token.bat
+++ Generate-Enrollment-Token.bat
@@ -1,4 +1,4 @@
 @echo off
 title Elasticsearch
 cd C:\apps\elasticsearch-8.0.0\bin
-elasticsearch-create-enrollment-token.bat --scope kibana
+cmd /k elasticsearch-create-enrollment-token.bat --scope kibana
--- day2-Lab-1.2-F&L.bat
+++ day2-Lab-1.2-F&L.bat
@@ -1,4 +1,7 @@
 #@echo off
+cd C:\apps\logstash-8.0.0\config
+mkdir certs
+xcopy /s C:\apps\elasticsearch-8.0.0\config\certs C:\apps\logstash-8.0.0\config\certs
 cd C:\apps
 start setup\scripts\filebeat.bat
 start setup\scripts\logstash.bat
